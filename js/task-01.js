02aaa9725a42d1b5d133acd0568cc3fd388c1c39
Pavlo-Zubovych <zu10@ukr.net>
2021-11-15 23:54:46 +0200

    add 1_(40%)

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const categoriesList = document.querySelector('#categories');
+const countCategories = categoriesList.children.length;
+console.log(`В списку ${countCategories} категорій`);
+
+console.log(categoriesList.children);
