818edda74be8d8a8181a18572438a13ce6ef3375
Pavlo-Zubovych <zu10@ukr.net>
2021-11-16 17:48:06 +0200

    done task_2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const createList = (ingredient) => {
+  const tagLiRef = document.createElement('li');
+  tagLiRef.textContent = ingredient;
+
+  const ingredientsList = document.querySelector('#ingredients');
+  ingredientsList.appendChild(tagLiRef);
+};
+
+ingredients.forEach((ingredient) => createList(ingredient));
+
+//перший варіант
+
+// const ingredientsList = document.querySelector('#ingredients');
+// // console.log(ingredients);
+
+// ingredients.forEach(function (ingredient) {
+//   const tagLiRef = document.createElement('li');
+//   tagLiRef.textContent = ingredient;
+
+//   ingredientsList.appendChild(tagLiRef);
+
+// });
